b6ac9bdfebc932c5d9ec8ca9a38be0e1aba9e6d7
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-02-21 02:36:27 +0200

    Edit .jsx files

--- src/Components/Hero/FileReader/FileReader.jsx
+++ src/Components/Hero/FileReader/FileReader.jsx
@@ -2,35 +2,63 @@ import { useState } from "react";
 
 export const FileRead = () => {
   const [selectedIMG, setSelectedIMG] = useState(null);
+  const [selectedSprite, setSelectedSprite] = useState(null);
   const [IMGSrc, setIMGSrc] = useState("");
+  const [spriteSrc, setSpriteSrc] = useState("");
 
-  const handleFileInputChange = (e) => {
+  const handleAddIMG = (e) => {
     const img = e.target.files[0];
     setSelectedIMG(img);
-    previewFile(img);
+    renderIMG(img);
+  };
+  const handleAddSprite = (e) => {
+    const sprite = e.target.files[0];
+    setSelectedSprite(sprite);
+    renderSprite(sprite);
   };
 
-  const previewFile = (img) => {
+  const renderIMG = (img) => {
     const reader = new FileReader();
     reader.readAsDataURL(img);
-    reader.onloadend = () => {
-      setIMGSrc(reader.result);
-    };
+    reader.onloadend = () => setIMGSrc(reader.result);
+  };
+
+  const renderSprite = (sprite) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(sprite);
+    reader.onloadend = () => setSpriteSrc(reader.result);
   };
 
   return (
-    <div className="FileReader">
-      <input type="file" id="inputImage" onChange={handleFileInputChange} />
+    <>
+      <div className="FileReader">
+        <label>
+          Фон:
+          <input type="file" id="inputImage" onChange={handleAddIMG} />
+        </label>
+        <label>
+          Спрайт:
+          <input type="file" id="inputSprite" onChange={handleAddSprite} />
+        </label>
+      </div>
       {selectedIMG && (
-        <div className="image-container">
+        <div className="image-container" id="result">
           <img
-            id="imagePreview"
             src={IMGSrc}
             alt="pic"
-            style={{ maxWidth: "100%" }}
+            style={{ maxWidth: "100%", height: "100%" }}
           />
+
+          {selectedSprite && (
+            <img
+              id="sprite"
+              src={spriteSrc}
+              alt="sprite"
+              style={{ maxWidth: "100%", height: "100%" }}
+            />
+          )}
         </div>
       )}
-    </div>
+    </>
   );
 };
